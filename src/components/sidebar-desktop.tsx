1a55e65538a229b849bda8c8cecbc414e2a89a2a
rohitd3 <de.rohit01@gmail.com>
2024-08-27 23:07:35 -0700

    removed extra

--- src/components/sidebar-desktop.tsx
+++ src/components/sidebar-desktop.tsx
@@ -10,67 +10,38 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { LogOut, MoreHorizontal, Settings } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 
+// Import the SEP logo
+import SepLogo from '../assets/logos/sep_white.png';
+
 interface SidebarDesktopProps {
   sidebarItems: SidebarItems;
 }
 
-
-
-
 export function SidebarDesktop(props: SidebarDesktopProps) {
-    const pathname = usePathname();
-    return(
-        <aside className='w-[270px] max-w-xs h-screen fixed left-0 top-0 z-40 border-r'>
-            <div className='h-full px-3 py-4'>
-                <h3 className='mx-3 text-lg font-semibold text-foreground'>Sigma Eta Pi</h3>
-                <div className='mt-5'>
-                <div className='flex flex-col gap-1 w-full'>
-                    {props.sidebarItems.links.map((link, index) => (
-                    <Link key={index} href={link.href}>
-                        <SidebarButton
-                        variant={pathname === link.href ? 'secondary' : 'ghost'}
-                        icon={link.icon}
-                        className='w-full'
-                        >
-                        {link.label}
-                        </SidebarButton>
-                    </Link>
-                    ))}
-                    {props.sidebarItems.extras}
-                </div>
-                <div className='absolute left-0 bottom-3 w-full px-3'>
-                    <Separator className='absolute -top-3 left-0 w-full' />
-                    <Popover>
-                    <PopoverTrigger asChild>
-                        <Button variant='ghost' className='w-full justify-start'>
-                        <div className='flex justify-between items-center w-full'>
-                            <div className='flex gap-2'>
-                            <Avatar className='h-5 w-5'>
-                                <AvatarImage src='https://github.com/max-programming.png' />
-                                <AvatarFallback>Max Programming</AvatarFallback>
-                            </Avatar>
-                            <span>Max Programming</span>
-                            </div>
-                            <MoreHorizontal size={20} />
-                        </div>
-                        </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className='mb-2 w-56 p-3 rounded-[1rem]'>
-                        <div className='space-y-1'>
-                        <Link href='/'>
-                            <SidebarButton size='sm' icon={Settings} className='w-full'>
-                            Account Settings
-                            </SidebarButton>
-                        </Link>
-                        <SidebarButton size='sm' icon={LogOut} className='w-full'>
-                            Log Out
-                        </SidebarButton>
-                        </div>
-                    </PopoverContent>
-                    </Popover>
-                </div>
-                </div>
-            </div>
-            </aside>
-    );
+  const pathname = usePathname();
+  return (
+    <aside className='w-[270px] max-w-xs h-screen fixed left-0 top-0 z-40 border-r'>
+      <div className='h-full px-3 py-4'>
+        {/* Replace the text with the SEP logo */}
+        <div className='mx-3'>
+          <img src={SepLogo.src} alt="SEP Logo" className='h-12 w-auto' />
+        </div>
+        <div className='mt-5'>
+          <div className='flex flex-col gap-1 w-full'>
+            {props.sidebarItems.links.map((link, index) => (
+              <Link key={index} href={link.href}>
+                <SidebarButton
+                  variant={pathname === link.href ? 'secondary' : 'ghost'}
+                  icon={link.icon}
+                  className='w-full'
+                >
+                  {link.label}
+                </SidebarButton>
+              </Link>
+            ))}
+          </div>           
+        </div>
+      </div>
+    </aside>
+  );
 }
