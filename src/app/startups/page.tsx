61899b1fd692b7473f4f081517c34907407d0be6
rohitd3 <de.rohit01@gmail.com>
2024-09-15 00:39:24 -0700

    removed startups_top

--- src/app/startups/page.tsx
+++ src/app/startups/page.tsx
@@ -1,11 +1,9 @@
 import FocusCards from "@/components/startups"
-import { BackgroundLinesDemo } from "@/components/startups_top";
 
 export default function ItemPage({ params }: { params: { item: string } }) {
     return (
       <div>
         <h1 className='text-3xl font-semibold capitalize'>{params.item} Page</h1>
-          <BackgroundLinesDemo/>
           <FocusCards />
       </div>
     );
