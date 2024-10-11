8bd7ee7f95e5bf74c3c4ff620ade9fca53b55d70
Ayaan Dhir <ayaand@uci.edu>
2024-10-10 17:27:03 -0700

    fixed a bug

--- src/components/FEComponents/fevp-section.tsx
+++ src/components/FEComponents/fevp-section.tsx
@@ -13,17 +13,18 @@ const Section: React.FC = () => {
             Founder&apos;s Education
           </h1>
           <p className="text-center md:text-2xl text-lg text-slate-300">
-          Founder's Education is focused on developing our newest members personally and professionally, 
-          by fostering a tight-knit community. Designed for all experience levels, FE is 
-          the go-to program for those of you at UCI looking to innovate, learn about the 
-          startup world, and meet some dynamic people!{" "}
+            Founder&apos;s Education is focused on developing our newest members
+            personally and professionally, by fostering a tight-knit community.
+            Designed for all experience levels, FE is the go-to program for
+            those of you at UCI looking to innovate, learn about the startup
+            world, and meet some dynamic people!{" "}
           </p>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <Image
             className="object-cover object-center rounded-md"
             alt="hero"
-            src={shreya2.src} 
+            src={shreya2.src}
             width={500} // Provide appropriate width for the image
             height={500} // Provide appropriate height for the image
             priority={true} // Optional: to prioritize this image for faster LCP
