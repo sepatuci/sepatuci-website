ee3b2b1552e494bbcc6c919254505bd4147b0017
Byren Cheema <cheemabyren@gmail.com>
2025-12-27 09:25:47 -0800

    chore: add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer";
